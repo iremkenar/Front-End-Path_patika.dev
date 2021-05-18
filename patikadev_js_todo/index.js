1327c9d55084c1272f965d94a8e07dfed4bffb29
iremkenar <irem.kenar@outlook.com>
2021-05-18 13:24:16 +0300

    Added add and delete functionality

---
+++ patikadev_js_todo/index.js
@@ -0,0 +1,40 @@
+const addBtn = document.querySelector('#liveToastBtn');
+const newTask = document.querySelector('#task');
+const taskList = document.querySelector('#list');
+const oldTask = taskList.querySelectorAll('li');
+
+function addSpan(task) {
+  const span = document.createElement('span');
+  span.classList.add('close');
+  span.innerHTML = 'x';
+  task.appendChild(span);
+}
+
+function addClose(task) {
+  task.forEach((task) => addSpan(task));
+}
+
+function addNewTask() {
+  const taskInputValue = newTask.value;
+  if (taskInputValue === '') {
+    alert('Add something');
+  } else {
+    const newTaskEl = document.createElement('li');
+    newTaskEl.innerHTML = taskInputValue;
+    taskList.appendChild(newTaskEl);
+    addSpan(newTaskEl);
+    newTask.value = '';
+  }
+}
+
+function deleteTask(e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+  } else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.style.display = 'none';
+  }
+}
+
+addBtn.addEventListener('click', addNewTask);
+taskList.addEventListener('click', deleteTask);
+addClose(oldTask);
